3960bb4712f02779c08257b582b53b9678b3248b
Saiful Islam <msik.milon@gmail.com>
2026-01-27 03:53:45 +0800

    clone from github

--- envision/settings.py
+++ envision/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+=)_&_mfq4n9*++gdwaky$xwgvo%^%q0ive&j%-ds52sz4cekx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  
+DEBUG = True  
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
