9092cffc331122e486c3e38767279523b03643e5
Saiful Islam <msik.milon@gmail.com>
2026-01-30 18:38:48 +0800

    production settings setup

--- envision/settings.py
+++ envision/settings.py
@@ -23,27 +23,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=)_&_mfq4n9*++gdwaky$xwgvo%^%q0ive&j%-ds52sz4cekx'
+# SECRET_KEY = 'django-insecure-+=)_&_mfq4n9*++gdwaky$xwgvo%^%q0ive&j%-ds52sz4cekx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True  
 
-ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 # coolify content start here
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 if not SECRET_KEY:
     raise Exception("SECRET_KEY is not set")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-# Optional but recommended
-# CSRF_TRUSTED_ORIGINS = [
-#     f"https://{host}" for host in ALLOWED_HOSTS if host
-# ]
+Optional but recommended
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{host}" for host in ALLOWED_HOSTS if host
+]
 # coolify content end here
 
 
